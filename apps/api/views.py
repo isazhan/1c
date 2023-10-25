ed4c176bf178428a13a5b8e705fe9b1d8e572209
ulan <u.isazhan@mail.ru>
2023-10-26 00:16:59 +0600

    first test with api

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def api(request):
+    if request.method == 'POST':
+        print(request.POST['test'])
+        return HttpResponse('good')    
--- onec/settings.py
+++ onec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=u@2w4+$dqn%6fo(o_yx(ve4u*!xp%#nbfbscegrpdmddfvxj3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
