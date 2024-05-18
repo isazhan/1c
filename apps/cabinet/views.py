72cf1a415523db793edc20abc031a6e30db47203
Ulan Isazhan <u.isazhan@mail.ru>
2024-05-18 16:55:02 +0500

    new arguments for driver

--- apps/cabinet/views.py
+++ apps/cabinet/views.py
@@ -106,6 +106,10 @@ def manage_driver(instance):
         options = webdriver.ChromeOptions()
         options.add_argument('incognito')
         options.add_argument('--headless=new')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-software-rasterizer')
         options.add_argument('user-agent=User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36')        
 
         # for linux:
