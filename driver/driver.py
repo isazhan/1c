b153071d1d51ac08143d49249cce588a6f57cccf
Ulan Isazhan <90261518+isazhan@users.noreply.github.com>
2024-06-14 21:16:37 +0500

    Update driver.py

--- driver/driver.py
+++ driver/driver.py
@@ -5,11 +5,11 @@ import time
       
 options = webdriver.ChromeOptions()
 options.add_argument('incognito')
-#options.add_argument('--headless=new')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-gpu')
-#options.add_argument('--disable-dev-shm-usage')
-#options.add_argument('--disable-software-rasterizer')
+options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-software-rasterizer')
 options.add_argument('user-agent=User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36')        
 
 service = webdriver.ChromeService(executable_path=binary_path)
@@ -164,83 +164,3 @@ while True:
             pass
 
     time.sleep(2)
-
-"""
-    status = ''
-    try:
-        if driver.current_url == 'https://web.whatsapp.com/':
-            try:
-                search_input = driver.find_element("xpath", "//div[@contenteditable='true'][@data-tab='3']")
-                status = 'auth'
-                value = {"$set": {"authcode": '', "authnumber": ''}}
-                x = col.update_one(query, value)
-                i = 0
-                try:
-                    col = db()['messages']
-                    doc = col.find_one(query)
-                        
-                    search_input.send_keys(doc['telnumber'])
-                    time.sleep(2)
-                    search_input.send_keys(webdriver.common.keys.Keys.RETURN)
-                    time.sleep(2)
-                    try:
-                        chat = driver.find_element("xpath", "//div[@class='_ajx_']")                            
-                        message_input = driver.find_element("xpath", "//div[@contenteditable='true'][@data-tab='10']")
-                        message_input.send_keys(doc['message'])
-                        message_input.send_keys(webdriver.common.keys.Keys.RETURN)
-                        time.sleep(2)
-                    except:
-                        pass
-                    print('test1')
-                    webdriver.ActionChains(driver).send_keys(webdriver.common.keys.Keys.ESCAPE).perform()
-                    print('test2')
-                    #driver.find_element("xpath", "//button[@class='_ah_y']").click()
-                    print('test3')
-                    time.sleep(doc['wait'])
-                    print(doc['wait'])
-                    print('test4')
-                    x = col.delete_one({'_id': doc['_id']})
-                    print('test5')
-                except:
-                    pass
-            except:
-                try:
-                    #qr = driver.find_element(webdriver.common.by.By.CLASS_NAME, "_akau")
-                    #qr = qr.get_attribute("data-ref")
-                    #value = {"$set": {"qr": qr}}
-                    #x = col.update_one(query, value)
-                    status = 'noauth'
-                    doc = db()['instances'].find_one({'instance': instance})
-                    if not doc['authnumber']=='':
-                        try:
-                            driver.find_element("xpath", "//span[@tabindex=0]").click()
-                            time.sleep(2)
-                            authnumber = driver.find_element("xpath", "//input[@aria-required='true']")
-                            authnumber.send_keys(doc['authnumber'])
-                            authnumber.send_keys(webdriver.common.keys.Keys.RETURN)
-                            time.sleep(1)
-                        except:
-                            pass
-                        authcode = driver.find_element("xpath", "//div[@aria-details='link-device-phone-number-code-screen-instructions']")
-                        authcode = authcode.get_attribute('data-link-code')
-                        value = {"$set": {"authcode": authcode}}
-                        x = col.update_one(query, value)
-                        i+=1
-                        print(i)
-                        if i==20:
-                            value = {"$set": {"authcode": '', "authnumber": ''}}
-                            x = col.update_one(query, value)
-                            driver.get('https://web.whatsapp.com/')
-                            i = 0
-                except:
-                    pass
-        else:
-            status = 'wrongurl'
-    except:
-        status = 'nodriver'
-
-    print(status)
-    status_update(instance, status)
-    time.sleep(2)
-    #print(driver.current_url)
-"""
